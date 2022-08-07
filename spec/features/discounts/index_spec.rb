608d689b42c2803556e7badc5a6dcc48b0dea78c
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-06 22:15:31 -0700

    troubleshooting delete button

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -21,10 +21,27 @@ RSpec.describe 'discounts index page' do
       expect(current_path).to eq("/merchant/#{@merchant1}/discounts/#{@discount10}")
     end
 
-   it 'has a link to create a new discount' do
+   xit 'has a link to create a new discount' do
       click_on('Create New Discount')
-      save_and_open_page
+   
       expect(current_path).to eq(new_merchant_discount_path(@merchant1))
+
+      fill_in(:quantity, with: 30)
+      fill_in(:percentage, with: 30)
+
+      click_on 'Create New Discount'
+
+      expect(page).to have_content('30%')
+      expect(page).to have_content(30)
    end
 
+   it 'has a link to delete a discount' do
+      @discount100 = @merchant1.discounts.create!(percentage: 100, quantity: 1)
+
+      click_on "Delete Discount", match: :first
+      
+      expect(page).to_not have_content(@discount100.percentage)
+      expect(page).to_not have_content(@discount100.quantity)
+      expect(current_page).to eq(merchant_discounts_path(@merchant1))
+   end
 end
