5a8acb94e8512c27e1bf3e6082f98f810302ceef
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-08 14:08:09 -0700

    edit button works us 5 complete

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -31,12 +31,12 @@ class DiscountsController < ApplicationController
     @discount = Discount.find(params[:id])
   end
 
-#   def update
-#     merchant = Merchant.find(params[:merchant_id])
-#     discount = Discount.find(params[:id])
-#     discount.update(discount_params) 
-#     redirect_to merchant_discount_path(merchant, discount)
-#   end
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.update(discount_params) 
+    redirect_to merchant_discount_path(merchant, discount)
+  end
 
 private
 
