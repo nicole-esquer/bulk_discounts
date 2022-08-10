e71a30ba2a3819f2e89a17ad08f2fb69f6097a83
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-10 00:15:04 -0700

    us 7 complete

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,11 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def discounts
+    item.merchant.discounts
+    .where('discounts.quantity <= ?', quantity)
+    .order('discounts.percentage desc')
+    .first
+  end
 end
